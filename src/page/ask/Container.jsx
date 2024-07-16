64645f609e11c958d1efbf4ea8877b08c99d1afd
blessedman-LJH <blessedjuhwan77@gamil.com>
2024-07-16 16:17:32 +0900

    feat:custom

--- src/page/ask/Container.jsx
+++ src/page/ask/Container.jsx
@@ -1,7 +1,8 @@
-import {Button, Card, Input, MenuItem, Select, TextField} from "@mui/material";
+import {Button, Card, Input, TextField} from "@mui/material";
 import React, { useState } from "react";
-
+import CustomSelect from "../../component/CustomSelect";
 export default function Container(){
+    
     const [age, setAge] = useState("");
     const [gender, setGender] = useState("남자");
     const [health, setHealth] = useState("주 1회");
@@ -24,16 +25,19 @@ export default function Container(){
         setStatus(e.target.value);
     };
     
-    const handleBtnClick = () =>{
-        const query = `나이는 ${encodeURIComponent(age)}세,
-        성별은 ${encodeURIComponent(gender)},
-        운동은 ${encodeURIComponent(health)},
-        기타 특이사항은 ${encodeURIComponent(status)}`;
-        console.log(query);
-    }
+    //const handleBtnClick = () =>{
+    //}
 
 
+    const genderArr = [{value : "남자",key:1},{value : "여자",key:2}];
+    const healthArr = [
+        {value : "주 1회",key : 1},
+        {value : "주 2회",key : 2},
+        {value : "주 3회",key : 3},
+        {value : "주 4회",key : 4},
+        {value : "주 5회",key : 5}
 
+    ]
 
     return(
         <div className="container Flex mt-32 text-center">
@@ -51,18 +55,20 @@ export default function Container(){
 
                     <div className="flex mt-4">
                         <label className="font-semibold mr-4 w-20">성별</label>
-                        <Select className="w-full" onChange={genderHandleChange}>
-                            <MenuItem value="남자">남자</MenuItem>
-                            <MenuItem value="여자">여자</MenuItem>
-                        </Select>
+                        <CustomSelect
+                            options={genderArr}
+                            onchange={genderHandleChange}
+                            value={gender}
+                        />
                     </div>
 
                     <div className="flex mt-4">
                         <label className="font-semibold mr-4 w-20">운동</label>
-                        <Select className="w-full" onChange={healthHandleChange}>
-                            <MenuItem value="주 1회">주 1회</MenuItem>
-                            <MenuItem value="주 2회">주 2회</MenuItem>
-                        </Select>
+                        <CustomSelect
+                            options={healthArr}
+                            onchange={healthHandleChange}
+                            value={health}
+                        />
                     </div>
 
                     <div className="flex mt-4">
@@ -80,7 +86,7 @@ export default function Container(){
                         <Button className="w-full"
                             variant="contained"
                             color="primary"
-                            onChange={handleBtnClick}>
+                            >
                         전송하기
                         </Button>
                     </div>
